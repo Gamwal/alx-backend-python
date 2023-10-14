1d93edc5b1b4dcba9791ee3a76c893e352be00ce
Gamaliel Adun <adungamaliel@gmail.com>
2023-10-14 22:54:06 +0100

    init

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Basic Annotations - add
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    type-annotated function add 
+
+    Args:
+        a (float): first float to be added
+        b (float): second float to be added
+
+    Returns:
+        sum (float): addition of a and b
+    """
+    return a + b

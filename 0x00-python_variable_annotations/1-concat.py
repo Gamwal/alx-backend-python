3680d0452abf82f85fbae937db65e4c2ab2e4a88
Gamaliel Adun <adungamaliel@gmail.com>
2023-10-14 23:01:01 +0100

    added 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Basic Annotations - concat
+"""
+
+
+def add(str1: str, str2: str) -> str:
+    """
+    type-annotated function concat
+
+    Args:
+        str1 (str): first string to be concatenated
+        str2 (str): second string to be concatenated
+
+    Returns:
+        sum (float): addition of a and b
+    """
+    return str1 + str2

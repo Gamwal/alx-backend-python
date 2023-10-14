dc5ace6948a077becfce0ac52665231dcf93e628
Gamaliel Adun <adungamaliel@gmail.com>
2023-10-14 23:06:09 +0100

    added 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Basic Annotations - floor
+"""
+
+import math
+
+
+def floor(n: float) -> float:
+    """
+    type-annotated function concat
+
+    Args:
+        n (float): number to find floor of
+        
+    Returns:
+        sum (float): floor of n
+    """
+    return math.floor(n)

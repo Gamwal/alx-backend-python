9e73c12f3f04cb3aedf219f7401f90231f778205
Gamaliel Adun <adungamaliel@gmail.com>
2023-10-22 11:21:55 +0100

    added 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Basic Annotations - to_str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    type-annotated function concat
+
+    Args:
+        n (float): number to find floor of
+
+    Returns:
+        string representation (str)
+    """
+    return str(n)
